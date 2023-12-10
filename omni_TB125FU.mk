d48fc11ce63a223ed6930b467f930075800b0c7b
ComradeWave <tomfoxy0852@protonmail.com>
2023-12-10 09:39:22 +0100

    Removed omni, testing if kernel fits

--- omni_TB125FU.mk
+++ omni_TB125FU.mk
@@ -6,11 +6,11 @@
 #
 
 # Inherit from those products. Most specific first.
- $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
- $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
- $(call inherit-product, device/lenovo/TB125FU/vendor/omni/config/common.mk)
+# $(call inherit-product, device/lenovo/TB125FU/vendor/omni/config/common.mk)
 
 # Inherit from TB125FU device
  $(call inherit-product, device/lenovo/TB125FU/device.mk)
