d9562a4b5c397dedf23754d54edf13f1501a5b3f
ComradeWave <94688733+ComradeWave@users.noreply.github.com>
2023-12-10 11:41:59 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
+TW_SCREEN_BLANK_ON_BOOT := false
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := false
 TW_NEVER_UNMOUNT_SYSTEM := true
