92e0648895f90bbcd0cab8892f8d740c3eaa486a
ComradeWave <tomfoxy0852@protonmail.com>
2023-12-11 16:13:23 +0100

    Modified BoardConfig.mk: Added TW_DEFAULT_LANGUAGE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,7 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := landscape_hdpi
 TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := en
 TW_SCREEN_BLANK_ON_BOOT := false
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
